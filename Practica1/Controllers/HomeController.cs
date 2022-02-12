67781a7531f7a0c796016e968013c7f6f628bd29
Mario Lúe <98698349+Mariolue26@users.noreply.github.com>
2022-02-11 19:47:43 -0600

    Se agrega el proyecto

---
+++ Practica1/Controllers/HomeController.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Practica1.Models;
+using System.Diagnostics;
+
+namespace Practica1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+        //Sin una acción no hay vista
+        public IActionResult Ejercicio(double number1, double number2)
+        {
+            if (number1>10 || number2 > 10)
+            {
+                return View("NoMayores");
+            }
+            Double result = number1 + number2;
+            ViewBag.Numbers1 = number1;
+            ViewBag.Numbers2 = number2;
+            ViewBag.VariableAEnviar=result;
+            return View();
+        }
+
+        public IActionResult NoMayores()
+        {
+            return View();
+        }
+
+        public IActionResult Multi(double number1, double number2)
+        {
+            if (number1 > 10 || number2 > 10)
+            {
+                return View("NoMayores");
+            }
+            Double result = number1 * number2;
+            ViewBag.Numbers1 = number1;
+            ViewBag.Numbers2 = number2;
+            ViewBag.VariableAEnviar = result;
+            return View();
+
+        }
+        
+        public IActionResult Resta(double number1, double number2)
+        {
+            if (number1 > 10 || number2 > 10)
+            {
+                return View("NoMayores");
+            }
+            Double result = number1 - number2;
+            ViewBag.Numbers1 = number1;
+            ViewBag.Numbers2 = number2;
+            ViewBag.VariableAEnviar = result;
+            return View();
+        }
+
+        public IActionResult Div(double number1, double number2)
+        {
+            if (number1 > 10 || number2 > 10)
+            {
+                return View("NoMayores");
+            }
+            Double result = number1 / number2;
+            ViewBag.Numbers1 = number1;
+            ViewBag.Numbers2 = number2;
+            ViewBag.VariableAEnviar = result;
+            return View();
+        }
+    }
+}
